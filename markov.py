4b66ed92d117d6d4db849b367c90665c552160f6
rivermont <william.11bennett@gmail.com>
2017-10-11 21:53:54 -0400

    Updated files.

--- markov.py
+++ markov.py
@@ -10,29 +10,29 @@ path = input('Folder to get pages from: ')
 # path = 'files/'
 
 with open('text.txt', 'w+') as f:
-	results = listdir(path)
-	for file in results:
-		if file.endswith('.html'):
-			b += 1
-			try:
-				with open(path + file, encoding="utf-8") as open_file:
-					try:
-						print('Digesting "{0}"'.format(file))
-						copyfileobj(open_file, f)
-					except UnicodeError:
-						print('Unknown encoding on file.')
-			except FileNotFoundError:
-				print('Invalid file name "{0}"'.format(file))
+    results = listdir(path)
+    for file in results:
+        if file.endswith('.html'):
+            b += 1
+            try:
+                with open(path + file, encoding="utf-8") as open_file:
+                    try:
+                        print('Digesting "{0}"'.format(file))
+                        copyfileobj(open_file, f)
+                    except UnicodeError:
+                        print('Unknown encoding on file.')
+            except FileNotFoundError:
+                print('Invalid file name "{0}"'.format(file))
 
 print('Reading huge_text.txt')
 with open('huge_text.txt', 'r') as f:
-	model = markovify.Text(f, retain_original=False)
+    model = markovify.Text(f, retain_original=False)
 
 print('Saving model to model.json')
 with open('model.json', 'w+') as f:
-	f.write(model.to_json())
+    f.write(model.to_json())
 
 print('Writing to markov1_nl.html')
 with open('markov1_nl.html', 'w+') as f:
-	for i in range(600):
-		f.write(model.make_sentence(tries=100))
+    for i in range(600):
+        f.write(model.make_sentence(tries=100))
